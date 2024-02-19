be4c38bbd9441962520072ab5977a5de148e0e02
gabo-di <gabriel.diaz.iturry@gmail.com>
2024-02-19 09:32:05 -0300

    proper testset for updating algorithms

--- test/splinglass_tests.jl
+++ test/splinglass_tests.jl
@@ -3,66 +3,35 @@ using Test
 using StableRNGs
 using LinearAlgebra
 
-@testset "Metropolis Hasting Update" begin
+@testset "Spin Glass Update Algorithms" begin
     rng = StableRNG(42)
     sze = 10;
     T = Float64;
     Beta = T(5);
 
+    algorithm_list=Dict(
+        :MetropolisHastings => MetropolisHastingsAlgorithm(),
+        :Glauber => GlauberAlgorithm(),
+        :Little => LittleAlgorithm()
+    )
     ising = SpinGlassIsingModel(T, sze, rng);
     ising.J .= 1/sze .* (ones(T,sze,sze) - LinearAlgebra.I);
     ising.H .= ones(T, sze);
-
-    y = zeros(T,sze);
     n = 100_000;
-    for i in 1:n
-        MetropolisHastingsUpdate!(ising, Beta, rng)
-        y .+= ising.s
+    y = zeros(T,sze);
+
+    @testset for algorithm_name in keys(algorithm_list)
+        algorithm = algorithm_list[algorithm_name]
+        y .= 0
+        ising.s .= rand(rng,ising._s,ising.sze)
+        for i in 1:n
+            updateIsingModel!(ising, Beta, algorithm, rng)
+            y .+= ising.s
+        end
+        y .= y./n
+        a =  tanh.(Beta*(ising.H + ising.J*y)) 
+        algorithm_name = string(algorithm_key)
+        @test isapprox(y,a; atol=1e-3) 
     end
-    y .= y./n
-    a =  tanh.(Beta*(ising.H + ising.J*y)) 
-    @test isapprox(y,a; atol=1e-3)
 end
 
-@testset "Little Update" begin
-    rng = StableRNG(42)
-    sze = 10;
-    T = Float64;
-    Beta = T(5);
-
-    ising = SpinGlassIsingModel(T, sze, rng);
-    ising.J .= 1/sze .* (ones(T,sze,sze) - LinearAlgebra.I);
-    ising.H .= ones(T, sze);
-
-    y = zeros(T,sze);
-    n = 100_000;
-    for i in 1:n
-        LittleUpdate!(ising, Beta, rng)
-        y .+= ising.s
-    end
-    y .= y./n
-    a =  tanh.(Beta*(ising.H + ising.J*y)) 
-    @test isapprox(y,a; atol=1e-3)
-end
-
-
-@testset "Glauber Update" begin
-    rng = StableRNG(42)
-    sze = 10;
-    T = Float64;
-    Beta = T(5);
-
-    ising = SpinGlassIsingModel(T, sze, rng);
-    ising.J .= 1/sze .* (ones(T,sze,sze) - LinearAlgebra.I);
-    ising.H .= ones(T, sze);
-
-    y = zeros(T,sze);
-    n = 100_000;
-    for i in 1:n
-        GlauberUpdate!(ising, Beta, rng)
-        y .+= ising.s
-    end
-    y .= y./n
-    a =  tanh.(Beta*(ising.H + ising.J*y)) 
-    @test isapprox(y,a; atol=1e-3)
-end
